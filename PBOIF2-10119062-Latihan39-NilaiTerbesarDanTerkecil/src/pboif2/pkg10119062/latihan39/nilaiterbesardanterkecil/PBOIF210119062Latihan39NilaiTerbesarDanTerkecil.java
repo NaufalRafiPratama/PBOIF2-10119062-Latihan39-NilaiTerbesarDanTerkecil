25928e524cc3a2fa6db8f6ad4112b9a3667fa26d
NaufalRafiPratama <73165365+NaufalRafiPratama@users.noreply.github.com>
2020-11-08 00:14:19 +0700

    Add files via upload

---
+++ PBOIF2-10119062-Latihan39-NilaiTerbesarDanTerkecil/src/pboif2/pkg10119062/latihan39/nilaiterbesardanterkecil/PBOIF210119062Latihan39NilaiTerbesarDanTerkecil.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119062.latihan39.nilaiterbesardanterkecil;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Program : program ini berisi program untuk menampilkan
+ * nilai terbesar dan terkecil
+ */
+public class PBOIF210119062Latihan39NilaiTerbesarDanTerkecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        NilaiTerbesarDanTerkecil nilaiMhs = new NilaiTerbesarDanTerkecil();
+        nilaiMhs.tampilNilaiMhs();
+    }
+    
+}
